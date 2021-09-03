b53fec93843bc35b913ea32df2a0b1c6de0b389c
JonathanYuri <86935880+JonathanYuri@users.noreply.github.com>
2021-09-03 16:47:15 -0300

    Update atividade1.c

--- atividade1.c
+++ atividade1.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 typedef struct no{
-    char letra;
-    int quant_filhos;
-    struct no *filhos[quant_filhos];
+    int valor;
+    struct no *left;
+    struct no *right;
 }No;
 
 void inicializar(No *raiz)
@@ -27,7 +27,7 @@ void imprimir(No *raiz)
 {
     if (raiz != NULL)
     {
-        printf("%i", raiz->letra);
+        printf("%i", raiz->valor);
         //printf("ESQUERDA\n");
         imprimir(raiz->left);
         //printf("DIREITA\n");
@@ -39,13 +39,13 @@ int pesquisa(No *raiz, int num)
 {
     if (raiz != NULL)
     {
-        if (raiz->letra == num)
+        if (raiz->valor == num)
         {
             return 1;
         }
         else
         {
-            if (num < raiz->letra)
+            if (num < raiz->valor)
             {
                 return pesquisa(raiz->left, num);
             }
@@ -58,12 +58,12 @@ int pesquisa(No *raiz, int num)
     return 0;
 }
 
-No *inserir(No *raiz, char letra, int cont)
+No *inserir(No *raiz, int num)
 {
     if (raiz == NULL)
     {
         No *aux = (No*) malloc(sizeof(No));   // 7 < 4  <--   --> 
-        aux->letra = num;
+        aux->valor = num;
         aux->left = NULL;
         aux->right = NULL;
         //printf("1");
@@ -72,59 +72,34 @@ No *inserir(No *raiz, char letra, int cont)
     else
     {
         //printf("2");
-        while()
-        raiz->filhos[cont] = inserir(raiz->filhos[cont], num, 0);
+        if (num > raiz->valor)
+        {
+            raiz->right = inserir(raiz->right, num);
+        }
+        else if (num < raiz->valor)
+        {
+            raiz->left = inserir(raiz->left, num);
+        }
     }
     return raiz;
-}
-
-int menu(){
-    int escolha;
-    printf("Digite '1' para adicionar um no a arvore\n");
-    printf("Digite '2' para imprimir a árvore\n");
-    printf("Digite '3' para finalizar o programa\n");
-
-    scanf("%i", &escolha);
-    return escolha;
 }
 
 int main()
 {
     No *raiz;
-    int num;
-    char letra;
 
     inicializar(raiz);
+    int num;
     
-    //raiz = inserir(raiz, 5);
-    num = menu();
-    while (num != 3)
-    {
-        if (num == 1)
-        {
-            // se o nó já existe (não vou deixar eles terem a mesma letra)
-            // se já existe eu adiciono nele, vou perguntar quantos filhos tem e talz
-            // quando eu for adicionar um nó eu pergunto o char dele e a quantidade de filhos, e o nome dos filhos
-            // talvez dê para mudar a quantidade de filhos dele dentro do inserir
-            printf("Digite o nome do elemento que deseja inserir:\n");
-            scanf("%c", &letra);
-            printf("Digite a quantidade de filhos que este no tem:\n");
-            scanf("%i", &num);
-
-            raiz = inserir(raiz, letra);
-        }
-        else if (num == 2)
-        {
-            imprimir(raiz);
-        }
-        else
-        {
-            printf("Valor Inválido\n");
-        }
-        num = menu();
-    }
-
-    /*
+    //for (int c = 0; c < 3 ; c++)
+    //{
+    //    scanf("%i", &num);
+    //    raiz = inserir(raiz, num);
+    //}
+    raiz = inserir(raiz, 7);
+    raiz = inserir(raiz, 9);
+    raiz = inserir(raiz, 4);
+    raiz = inserir(raiz, 5);
     int a = pesquisa(raiz, 4);
     if (a == 0)
     {
@@ -136,7 +111,6 @@ int main()
     }
     
     imprimir(raiz);
-    */
     liberar(raiz);
 
     return 0;
