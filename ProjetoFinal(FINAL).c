e76dc159f8cd6e2c872da7da8eccb83d55b14e31
JonathanYuri <86935880+JonathanYuri@users.noreply.github.com>
2021-09-14 19:18:52 -0300

    Adicionar um carro, e colisão com carros
    trocar de lugar o carro 2 com o 1

--- ProjetoFinal(FINAL).c
+++ ProjetoFinal(FINAL).c
@@ -1,9 +1,15 @@
 #include <stdio.h>
+#include <ctype.h>
+
+// transformar o y, z, a e b numa matriz e o andou num array
 
 //cima = 1, baixo = 2, esq = 3, dir = 4
 typedef struct{
     char r[2];
 }locura;
+
+// para verificar se ja tem um carro ali, é mais facil verificar se não existe uma rua naquele lugar
+// pq sim
 
 void Quadra (locura quadra[][5], char rua[])
 {
@@ -56,9 +62,14 @@ void Quadra (locura quadra[][5], char rua[])
     quadra[3][4].r[0] = 'b';
 }
 
+int verificar_carro()
+{
+    
+}
+
 int main()
 {
-    int y, z, andou, a, b, k = 0;
+    int andou, andou2, k = 0, y, z, a, b;
     int quant_linhas = 4;
     int quant_colunas = 5;
     
@@ -132,7 +143,7 @@ int main()
             printf("\n");
         }
         printf("\n");
-            
+        // carro[x][1] == '1'
         if (carro[11][1] == '1') //final 1 mesmo padrão
         {
             //Se o seqMov1[0] falhar, olhamos o próximo até o seqMov1[4], "return" o move true
@@ -145,7 +156,7 @@ int main()
                 //printf("VALOR %i\n", y - 1);
                 if ((y - 1) >= 0)  // se ele tentar chegar -1 paro
                 {
-                    if (quadra[y-1][z].r[0] != 'x')
+                    if (quadra[y-1][z].r[0] != 'x' && isalpha(quadra[y-1][z].r[0])) // uma letra diferente de x
                     {
                         // guardar o carro
                             
@@ -171,7 +182,7 @@ int main()
                 //printf("VALOR %i\n", z - 1);
                 if ((z - 1) >= 0)
                 {
-                    if (quadra[y][z-1].r[0] != 'x')
+                    if (quadra[y][z-1].r[0] != 'x' && isalpha(quadra[y][z-1].r[0]))
                     {
                         // guardar o carro
                             
@@ -197,7 +208,7 @@ int main()
                 //printf("VALOR %i\n", y + 1);
                 if ((y + 1) < quant_linhas)
                 {
-                    if (quadra[y+1][z].r[0] != 'x')
+                    if (quadra[y+1][z].r[0] != 'x' && isalpha(quadra[y+1][z].r[0]))
                     {
                         // guardar o carro
                             
@@ -223,7 +234,7 @@ int main()
                 //printf("VALOR %i\n", z + 1);
                 if ((z + 1) < quant_colunas)
                 {
-                    if (quadra[y][z+1].r[0] != 'x')
+                    if (quadra[y][z+1].r[0] != 'x' && isalpha(quadra[y][z+1].r[0]))
                     {
                         // guardar o carro
                             
@@ -239,6 +250,114 @@ int main()
                         
                         z = z + 1;
                         andou = 1;
+                    }
+                }
+            }
+        }
+        if (carro[12][1] == '2')
+        {
+            andou2 = 0;
+            if ((seqMov[1][0] == R_atual[1][0] || seqMov[1][0] == R_atual[1][1]) && andou2 == 0)
+            {
+                // cima
+                //printf("VALOR %i\n", y - 1);
+                if ((a - 1) >= 0)  // se ele tentar chegar -1 paro
+                {
+                    // tenho que verificar se ela pode mover de acordo com o sentido da rua
+                    if (quadra[a-1][b].r[0] != 'x' && isalpha(quadra[a-1][b].r[0]))
+                    {
+                        // guardar o carro
+                            
+                        // vou andar, a que eu estava vai pegar o valor
+                        quadra[a][b].r[0] = R_atual[1][0];
+                        quadra[a][b].r[1] = R_atual[1][1];
+                        // vou guardar o valor da q vai estar o carro
+                        R_atual[1][0] = quadra[a-1][b].r[0];
+                        R_atual[1][1] = quadra[a-1][b].r[1];
+                        // carro vai andar
+                        quadra[a-1][b].r[0] = carro[12][0];
+                        quadra[a-1][b].r[1] = carro[12][1];
+                        
+                        a = a - 1;
+                        andou2 = 1;
+                    }
+                }
+            }
+            //printf("%c %c %i\n", R_atual[0], R_atual[1], andou);
+            if ((seqMov[1][1] == R_atual[1][0] || seqMov[1][1] == R_atual[1][1]) && andou2 == 0)
+            {
+                // dir
+                //printf("VALOR %i\n", z - 1);
+                if ((b + 1) >= 0)
+                {
+                    if (quadra[a][b+1].r[0] != 'x' && isalpha(quadra[a][b+1].r[0]))
+                    {
+                        // guardar o carro
+                            
+                        // vou andar, a que eu estava vai pegar o valor
+                        quadra[a][b].r[0] = R_atual[1][0];
+                        quadra[a][b].r[1] = R_atual[1][1];
+                        // vou guardar o valor da q vai estar o carro
+                        R_atual[1][0] = quadra[a][b+1].r[0];
+                        R_atual[1][1] = quadra[a][b+1].r[1];
+                        // carro vai andar
+                        quadra[a][b+1].r[0] = carro[12][0];
+                        quadra[a][b+1].r[1] = carro[12][1];
+                        
+                        b = b + 1;
+                        andou2 = 1;
+                    }
+                }
+            }
+            //printf("%c %c %i\n", R_atual[0], R_atual[1], andou);
+            if ((seqMov[1][2] == R_atual[1][0] || seqMov[1][2] == R_atual[1][1]) && andou2 == 0)
+            {
+                // baixo
+                //printf("VALOR %i\n", y + 1);
+                if ((a + 1) < quant_linhas)
+                {
+                    if (quadra[a+1][b].r[0] != 'x' && isalpha(quadra[a+1][b].r[0]))
+                    {
+                        // guardar o carro
+                            
+                        // vou andar, a que eu estava vai pegar o valor
+                        quadra[a][b].r[0] = R_atual[1][0];
+                        quadra[a][b].r[1] = R_atual[1][1];
+                        // vou guardar o valor da q vai estar o carro
+                        R_atual[1][0] = quadra[a+1][b].r[0];
+                        R_atual[1][1] = quadra[a+1][b].r[1];
+                        // carro vai andar
+                        quadra[a+1][b].r[0] = carro[12][0];
+                        quadra[a+1][b].r[1] = carro[12][1];
+                        
+                        a = a + 1;
+                        andou2 = 1;
+                    }
+                }
+            }
+            //printf("%c %c %i\n", R_atual[0], R_atual[1], andou);
+            if ((seqMov[1][3] == R_atual[1][0] || seqMov[1][3] == R_atual[1][1]) && andou2 == 0)
+            {
+                // esq
+                //printf("VALOR %i\n", z + 1);
+                if ((b - 1) < quant_colunas)
+                {
+                    if (quadra[a][b-1].r[0] != 'x' && isalpha(quadra[a][b-1].r[0]))
+                    {
+                        // guardar o carro
+                            
+                        // vou andar, a que eu estava vai pegar o valor
+                        quadra[a][b].r[0] = R_atual[1][0];
+                        quadra[a][b].r[1] = R_atual[1][1];
+                        // vou guardar o valor da q vai estar o carro
+                        R_atual[1][0] = quadra[a][b-1].r[0];
+                        R_atual[1][1] = quadra[a][b-1].r[1];
+                        // carro vai andar
+                        quadra[a][b-1].r[0] = carro[12][0];
+                        quadra[a][b-1].r[1] = carro[12][1];
+                        
+                        b = b - 1;
+                        andou2 = 1;
                     }
                 }
             }
