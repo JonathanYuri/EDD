2f028a58c7b121fed1e256a249bf0cbfc2e9aeb3
JonathanYuri <86935880+JonathanYuri@users.noreply.github.com>
2021-09-29 22:43:49 -0300

    Update caixeiro_viajante.c

--- caixeiro_viajante.c
+++ caixeiro_viajante.c
@@ -33,20 +33,52 @@ int verificar_num()
     }
 }
 
+int verificar_igual(int vetor[], int num, int cont)
+{
+    if (cont == 5)
+    {
+        return 0;
+    }
+    if (num == vetor[cont])
+    {
+        return 1;
+    }
+    else
+    {
+        return verificar_igual(vetor, num, cont+1);
+    }
+}
+
+int quant_percorrida(int vetor[], int cont, int qnt)
+{
+    if (cont == 5)
+    {
+        return qnt;
+    }
+    if (vetor[cont] != 0)
+    {
+        qnt++;
+    }
+    else
+    {
+        return quant_percorrida(vetor, cont + 1, qnt);
+    }
+}
+
 void imprimir(No *raiz)
 {
     if (raiz != NULL)
     {
         printf("%i\n", raiz->valor);
-        printf("((0)) ");
+        //printf("((0)) ");
         imprimir(raiz->prox[0]);
-        printf("((1)) ");
+        //printf("((1)) ");
         imprimir(raiz->prox[1]);
-        printf("((2)) ");
+        //printf("((2)) ");
         imprimir(raiz->prox[2]);
-        printf("((3)) ");
+        //printf("((3)) ");
         imprimir(raiz->prox[3]);
-        printf("SAIU\n");
+        //printf("SAIU\n");
     }
 }
 
@@ -56,7 +88,7 @@ No *inserir(No *raiz, int num)
     if (raiz == NULL)
     {
         // INSERIR
-        printf("ENTROU\n");
+        //printf("ENTROU\n");
         No *aux = (No*) malloc(sizeof(No));
         int qnt, valor_adj, custo;
         
@@ -156,10 +188,51 @@ int pesquisa_adj(No *raiz, int num)
     return 0;
 }
 
+void percorrer(No *raiz, int percorridos[], int posicao_atual, int posicao_inicial, int cont, int custo_total)
+{
+    int passou, p;
+    
+    if (cont != 0 && (posicao_atual == posicao_inicial))
+    {
+        printf("ACABOU\n");
+        printf("===%i===", custo_total);
+        return;
+    }
+    if (raiz->valor == posicao_atual)
+    {
+        printf("NA POSICAO CERTA\n");
+        printf("ADJACENCIAS: \n");
+        for (int i = 0; i < 4; i++)
+        {
+            printf("(%i %i) ", raiz->adjacencia[i].num_adj, raiz->adjacencia[i].custo);
+        }
+        
+        passou = verificar_igual(percorridos, raiz->adjacencia[0].num_adj, 0);
+        //printf("((%i))", passou);
+        
+        if (passou == 0) // nao passou nele AINDA
+        {
+            custo_total += raiz->adjacencia[0].custo;
+            
+            p = quant_percorrida(percorridos, 0, 0);
+            percorridos[p] = raiz->adjacencia[0].num_adj;   // percorridos[n] vou ter que achar quantos foram escritos
+            percorrer(raiz->prox[0], percorridos, percorridos[p], posicao_inicial, cont + 1, custo_total);
+        }
+        printf("\n");
+    }
+    else
+    {
+        percorrer(raiz->prox[0], percorridos, posicao_atual, posicao_inicial, cont + 1, custo_total);
+        percorrer(raiz->prox[1], percorridos, posicao_atual, posicao_inicial, cont + 1, custo_total);
+        percorrer(raiz->prox[2], percorridos, posicao_atual, posicao_inicial, cont + 1, custo_total);
+        percorrer(raiz->prox[3], percorridos, posicao_atual, posicao_inicial, cont + 1, custo_total);
+    }
+}
+
 int main()
 {
     No *raiz;
-    int num, vetor[4];
+    int num, percorridos[5] = {0, 0, 0, 0, 0};
     
     raiz = NULL;
     //inicializar(raiz);
@@ -167,7 +240,6 @@ int main()
     // inserir o primeiro elemento
     printf("Digite o numero a ser inserido: ");
     num = verificar_num();
-    
     raiz = inserir(raiz, num);
     
     //INSERIU
@@ -187,11 +259,8 @@ int main()
     //printf("EXISTE %i\n", existe);
     imprimir(raiz);
     
-    /*
-    for (int c = 0; c < 5; c++)
-    {
-        
-    }*/
+    percorridos[0] = 1;
+    percorrer(raiz, percorridos, 1, 1, 0, 0);
     
     /*na inserir eu antes de colocar ele em algum lugar, eu preciso saber se ele ja existe nas listas de adjacencia
     se ele nao existe e a raiz tiver nao nula eu nao posso adicionar, se ele ja existe, eu tenho que colocar ele 
