e93444d7baedd48cab0f0035367f219be4f46631
JonathanYuri <86935880+JonathanYuri@users.noreply.github.com>
2021-10-03 18:58:16 -0300

    Update caixeiro_viajante.c

--- caixeiro_viajante.c
+++ caixeiro_viajante.c
@@ -51,7 +51,8 @@ int verificar_igual(int vetor[], int num, int cont)
 
 int quant_percorrida(int vetor[], int cont, int qnt)
 {
-    if (cont == 5)
+    //printf("cont %i valor %i\n", cont, vetor[cont]);
+    if (cont == 4)
     {
         return qnt;
     }
@@ -59,10 +60,8 @@ int quant_percorrida(int vetor[], int cont, int qnt)
     {
         qnt++;
     }
-    else
-    {
-        return quant_percorrida(vetor, cont + 1, qnt);
-    }
+    
+    return quant_percorrida(vetor, cont + 1, qnt);
 }
 
 void imprimir(No *raiz)
@@ -70,15 +69,15 @@ void imprimir(No *raiz)
     if (raiz != NULL)
     {
         printf("%i\n", raiz->valor);
-        //printf("((0)) ");
+        printf("((0)) ");
         imprimir(raiz->prox[0]);
-        //printf("((1)) ");
+        printf("((1)) ");
         imprimir(raiz->prox[1]);
-        //printf("((2)) ");
+        printf("((2)) ");
         imprimir(raiz->prox[2]);
-        //printf("((3)) ");
+        printf("((3)) ");
         imprimir(raiz->prox[3]);
-        //printf("SAIU\n");
+        printf("SAIU\n");
     }
 }
 
@@ -188,10 +187,106 @@ int pesquisa_adj(No *raiz, int num)
     return 0;
 }
 
-void percorrer(No *raiz, int percorridos[], int posicao_atual, int posicao_inicial, int cont, int custo_total)
+No *pesquisa(No *raiz, int procurar)
+{
+    int c = 0;
+    No *aux = (No*) malloc (sizeof(No));
+    aux = NULL;
+    printf("%i ", procurar);
+    
+    if (raiz == NULL)
+    {
+        printf("GGGGG");
+    }
+    if (raiz->valor == procurar)
+    {
+        printf("ACHOU");
+        return raiz;
+    }
+    else
+    {
+        printf("11111 ");
+        
+        if (procurar == 2 || procurar == 3)
+        {
+            aux = pesquisa(raiz->prox[0], procurar);
+            if (aux != NULL)
+            {
+                return aux;
+            }
+        }
+        else if (procurar == 4)
+        {
+            aux = pesquisa(raiz->prox[1], procurar);
+            if (aux != NULL)
+            {
+                return aux;
+            }
+        }
+        else if (procurar == 5)
+        {
+            aux = pesquisa(raiz->prox[2], procurar);
+            if (aux != NULL)
+            {
+                return aux;
+            }
+        }
+        
+        //aux = pesquisa(raiz->prox[0], procurar);
+        //if (aux != NULL)
+        //{
+        //    return aux;
+        //}
+        /*
+        aux = pesquisa(raiz->prox[1], procurar);
+        if (aux != NULL)
+        {
+            return aux;
+        }
+        pesquisa(raiz->prox[2], procurar);*/
+        /*
+        pesquisa(raiz->prox[0], procurar);
+        while(raiz->prox[c] == NULL)
+        {
+            pesquisa(raiz->prox[c], procurar);
+            c++;
+        }*/
+    }
+    return raiz;
+}
+
+void percorrer(No *inicio, No *raiz, int percorridos[], int posicao_atual, int posicao_inicial, int cont, int custo_total)
 {
     int passou, p, c = 0;
     
+    p = quant_percorrida(percorridos, 0, 0);
+    printf("---------%i-------", p);
+    
+    /*
+    if (cont == 3)
+    {
+        printf("%i %i -- %i (%i)", posicao_atual, posicao_inicial, passou, c);
+        return;
+    }
+    */
+    if (p == 4)
+    {
+        // so falta retornar pro inicio
+        // em alguns casos eu nao conseguiria voltar para o inicio, por exemplo comecando no 1 e terminando no 3 (ja que o 3 nao tem ligacao com o 1)
+        // outro exemplo seria comecando no 3 ou no 2 ou no 5
+        // so nao aconteceria no 4 pq o 4 tem ligacao com todos
+        
+        
+        printf("ADSASDADAS");
+        return;
+    }
+    /*
+    if (cont == 1)
+    {
+        printf("%i (%i %i)\n", cont, posicao_atual, posicao_inicial);
+        printf("%i\n", raiz->valor);
+        return;
+    }*/
     if (cont != 0 && (posicao_atual == posicao_inicial))
     {
         printf("ACABOU\n");
@@ -202,6 +297,7 @@ void percorrer(No *raiz, int percorridos[], int posicao_atual, int posicao_inici
     {
         printf("NA POSICAO CERTA\n");
         printf("ADJACENCIAS: \n");
+        
         for (int i = 0; i < 4; i++)
         {
             printf("(%i %i) ", raiz->adjacencia[i].num_adj, raiz->adjacencia[i].custo);
@@ -209,7 +305,6 @@ void percorrer(No *raiz, int percorridos[], int posicao_atual, int posicao_inici
         
         passou = verificar_igual(percorridos, raiz->adjacencia[c].num_adj, 0);
         
-        p = quant_percorrida(percorridos, 0, 0);
         while (passou == 1 && c < 3)
         {
             c++;
@@ -218,18 +313,42 @@ void percorrer(No *raiz, int percorridos[], int posicao_atual, int posicao_inici
         
         custo_total += raiz->adjacencia[c].custo;
         percorridos[p] = raiz->adjacencia[c].num_adj;   // percorridos[n] vou ter que achar quantos foram escritos
+        
+        
         printf("%i %i %i %i %i\n", percorridos[0], percorridos[1], percorridos[2], percorridos[3], percorridos[4]);
         
-        printf("%i", raiz->prox[c]->valor);
-        //percorrer(raiz, percorridos, percorridos[p], posicao_inicial, cont + 1, custo_total);
-        printf("\n");
-    }
-    else
-    {
-        percorrer(raiz->prox[0], percorridos, posicao_atual, posicao_inicial, cont + 1, custo_total);
-        percorrer(raiz->prox[1], percorridos, posicao_atual, posicao_inicial, cont + 1, custo_total);
-        percorrer(raiz->prox[2], percorridos, posicao_atual, posicao_inicial, cont + 1, custo_total);
-        percorrer(raiz->prox[3], percorridos, posicao_atual, posicao_inicial, cont + 1, custo_total);
+        if (raiz->prox[c] == NULL)
+        {
+            printf("====%i====", percorridos[p]);
+            raiz = pesquisa(inicio, percorridos[p]);
+            
+            percorrer(inicio, raiz, percorridos, percorridos[p], posicao_inicial, cont + 1, custo_total);
+        }
+        else
+        {
+            printf("%i", raiz->prox[c]->valor);
+            
+            percorrer(inicio, raiz->prox[c], percorridos, percorridos[p], posicao_inicial, cont + 1, custo_total);
+            
+            printf("\n");
+        }
+        /*
+        if (cont == 2)
+        {
+            //printf("%s", raiz->prox[c]); // null
+            
+            //printf("-=0 %i 0=-", raiz->prox[c]->valor);
+            
+            //printf("\ndentro da fun %i", raiz->valor);
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    printf("(%i %i) ", raiz->adjacencia[i].num_adj, raiz->adjacencia[i].custo);
+            //}
+            
+            //printf("raiz %i", raiz->valor);
+            //
+            return;
+        }*/
     }
 }
 
@@ -249,6 +368,9 @@ int main()
     //INSERIU
     //imprimir(raiz);
     //printf("\n%i\n", raiz->valor);
+    
+    //printf("((%i %i %i %i %i))", raiz->valor, raiz->prox[0]->valor, raiz->prox[0]->prox[0]->valor, raiz->prox[1]->valor, raiz->prox[2]->valor);
+    
     for (int c = 0; c < 4; c++)
     {
         printf("\nDigite o numero a ser inserido: ");
@@ -264,7 +386,7 @@ int main()
     imprimir(raiz);
     
     percorridos[0] = 1;
-    percorrer(raiz, percorridos, 1, 1, 0, 0);
+    percorrer(raiz, raiz, percorridos, 1, 1, 0, 0);
     
     /*na inserir eu antes de colocar ele em algum lugar, eu preciso saber se ele ja existe nas listas de adjacencia
     se ele nao existe e a raiz tiver nao nula eu nao posso adicionar, se ele ja existe, eu tenho que colocar ele 
@@ -273,4 +395,5 @@ int main()
     return 0;
 }
 
-// ta dando problema para andar ainda
+// o que esta acontecendo, eu tenho a arvore e talz, mas quando eu estou procurando
+// por exemplo estou no 3 e quero ir pro 4 eu nao posso simplesmente avancar pq o 4 nao é filho do 3, entao eu tenho que voltar procurando o 4
